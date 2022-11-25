c9d1ee5da9ae9d0ec3076f702921768fe7bd3d72
Halim Bilici <halimbilici2022@gmail.com>
2022-11-25 23:19:45 +0300

    OOP Refactoring

---
+++ Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string Authorization { get; set; }
+        public string Active { get; set; }
+    }
+}
